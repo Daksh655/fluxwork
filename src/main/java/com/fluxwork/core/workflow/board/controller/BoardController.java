baa7cf7327f78b28a9d59e535bab7577415b5650
Daksh <daksh.tiwari655@gmail.com>
2026-05-24 13:38:42 +0530

    feat: Completed full board CRUD workflow module

--- src/main/java/com/fluxwork/core/workflow/board/controller/BoardController.java
+++ src/main/java/com/fluxwork/core/workflow/board/controller/BoardController.java
@@ -1,5 +1,6 @@
 package com.fluxwork.core.workflow.board.controller;
 
+import java.util.List;
 import com.fluxwork.core.common.response.ApiResponse;
 import com.fluxwork.core.workflow.board.dto.BoardRequest;
 import com.fluxwork.core.workflow.board.dto.BoardResponse;
@@ -23,5 +24,37 @@ public class BoardController {
         BoardResponse response = boardService.createBoard(request);
 
         return ApiResponse.success(response, "Board created successfully");
+    }
+
+    @GetMapping // GET request
+    public ApiResponse<List<BoardResponse>> getAllBoards() {
+
+        List<BoardResponse> response = boardService.getAllBoards();
+
+        return ApiResponse.success(response, "Boards fetched successfully");
+    }
+
+    @PutMapping("/{id}") // PUT request by ID
+    public ApiResponse<BoardResponse> updateBoard(
+            @PathVariable Long id,
+            @RequestBody BoardRequest request
+    ) {
+
+        BoardResponse response = boardService.updateBoard(id, request);
+
+        return ApiResponse.success(response, "Board updated successfully");
+    }
+
+    @DeleteMapping("/{id}") // DELETE request by ID
+    public ApiResponse<String> deleteBoard(
+            @PathVariable Long id
+    ) {
+
+        boardService.deleteBoard(id);
+
+        return ApiResponse.success(
+                "Board deleted",
+                "Board deleted successfully"
+        );
     }
 }
--- src/main/java/com/fluxwork/core/workflow/board/service/BoardService.java
+++ src/main/java/com/fluxwork/core/workflow/board/service/BoardService.java
@@ -1,5 +1,7 @@
 package com.fluxwork.core.workflow.board.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import com.fluxwork.core.workflow.board.dto.BoardRequest;
 import com.fluxwork.core.workflow.board.dto.BoardResponse;
 import com.fluxwork.core.workflow.board.entity.BoardEntity;
@@ -13,6 +15,7 @@ public class BoardService {
 
     private final BoardRepository boardRepository;
 
+    // this is the creat a new board
     public BoardResponse createBoard(BoardRequest request) {
 
         BoardEntity board = new BoardEntity();
@@ -30,5 +33,54 @@ public class BoardService {
         response.setCreatedAt(savedBoard.getCreatedAt());
 
         return response;
+    }
+
+    // this is to find all the board form DB
+    public List<BoardResponse> getAllBoards() {
+
+        List<BoardEntity> boards = boardRepository.findAll(); // fetch all board
+
+        return boards.stream().map(board -> { // convert boardEntity to boardResponse
+
+            BoardResponse response = new BoardResponse();
+
+            response.setId(board.getId());
+            response.setName(board.getName());
+            response.setDescription(board.getDescription());
+            response.setCreatedAt(board.getCreatedAt());
+
+            return response;
+
+        }).collect(Collectors.toList()); // Converts stream back into List
+    }
+
+    // this is the update the board by using ID
+    public BoardResponse updateBoard(Long id, BoardRequest request) {
+
+        BoardEntity board = boardRepository.findById(id) // it will fetch board from DB using primary key
+                .orElseThrow(() -> new RuntimeException("Board not found"));
+
+        board.setName(request.getName());
+        board.setDescription(request.getDescription());
+
+        BoardEntity updatedBoard = boardRepository.save(board);
+
+        BoardResponse response = new BoardResponse();
+
+        response.setId(updatedBoard.getId());
+        response.setName(updatedBoard.getName());
+        response.setDescription(updatedBoard.getDescription());
+        response.setCreatedAt(updatedBoard.getCreatedAt());
+
+        return response;
+    }
+
+    // this is to delete a board using id
+    public void deleteBoard(Long id) {
+
+        BoardEntity board = boardRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Board not found"));
+
+        boardRepository.delete(board); // auto generate sql query of deletion
     }
 }
